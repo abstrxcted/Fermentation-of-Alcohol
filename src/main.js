8d49af8c9b7117e25c55c7840a6b269f09dd81d1
Roman <cda0420@gmail.com>
2023-12-12 16:31:05 -0500

    little changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as GSAP from 'gsap';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -9,11 +11,13 @@ const loader  = new GLTFLoader();
 const red_light = new THREE.DirectionalLight(0xff0000, 50);
 const white_light = new THREE.AmbientLight(0x404040, 25.0);
 const controls = new OrbitControls(camera, renderer.domElement);
+const material1 = new THREE.LineBasicMaterial({color:0x000ff});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 controls.autoRotate = false;
 controls.enableZoom = true;
+controls.enablePan = false;
 
 
 window.addEventListener('resize', function(){
@@ -31,7 +35,7 @@ loader.load('res/Ethanol Basic/ethanol.glb', function(gltf) {
     scene.add(model);
     ethanol = model;
     ethanol.scale.set(0.25, 0.25, 0.25);
-    ethanol.position.set(-5, 0, 0);
+    ethanol.position.set(-5, 0.5, 0);
     
 
 }, undefined, function (error) {
@@ -50,19 +54,26 @@ loader.load('res/Dragon Test/shadow_dragon.glb', function(gltf) {
     console.error(error);
 } );
 
-// cube creation
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+const geometry1 = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry1, material1 );
+scene.add( line );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color:0x003000});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-cube.scale.set(1, 1, 1);
-cube.position.set(5, 0, 0);
+cube.scale.set(100, 0, 100);
+cube.position.set(0, 0, 0);
 red_light.position.set(-1000,1000,1000)
 scene.add(red_light);
 scene.add(red_light.target);
 scene.add(white_light)
 camera.position.z = 5;
+camera.position.y = 1;
 controls.update();
 
 function animate() {
